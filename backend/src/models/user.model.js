e56ba974d3eba76e667bdbc5cd75bac823361f19
Alexi Reyes <alexireyes6796@gmail.com>
2025-10-15 11:56:04 +0200

    added user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    profilePicture: {
+        type: String,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
