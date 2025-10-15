d6130ed196ff1c0be1d4990de24061caeb043ea8
blaise duplantier <blaise.duplantier@gmail.com>
2025-10-15 14:43:47 +0200

    ajout page connexion et inscription

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 
 import Navbar from './components/Navbar.vue' 
+import Connexion from './components/Connexion.vue';
 </script>
 
 <template>
   <Navbar />
-  
+  <Connexion />
   
 </template>
 
