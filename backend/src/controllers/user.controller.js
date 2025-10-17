ee659e970cee4d9fe94bf4ef9fadb4f5812f0d2a
Alexi Reyes <alexireyes6796@gmail.com>
2025-10-17 16:08:15 +0200

    update path cookie

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -121,7 +121,7 @@ class UserController {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
                 path: "/",
-                domain: "*",
+                sameSite: "none",
                 maxAge: 1000 * 60 * 60 * 24,
             });
 
